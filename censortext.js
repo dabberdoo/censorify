fb53d05b4a6e91e64ff90b890ce9fe385efa3e12
dabberdoo <steve_davis01@hotmail.com>
2016-02-09 10:48:42 +0000

    First build of project

---
+++ censortext.js
@@ -0,0 +1,23 @@
+/**
+ * Created by steve_davis01 on 09/02/2016.
+ */
+var censoredWords = ["sad", "bad", "mad"];
+var customCensorWords = [];
+function censor(inStr) {
+    for (idx in censorWords) {
+        inStr = inStr.replace(censoredWords[idx], "****");
+    }
+    for (idx in customCensorWords) {
+        inStr = inStr.replace(customCensorWords[idx], "****");
+    }
+    return inStr;
+}
+function addCensoredWord(word) {
+    customCensorWords.push(word);
+}
+function getCensoredWords() {
+    return censoredWords.concat(customCensorWords);
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
